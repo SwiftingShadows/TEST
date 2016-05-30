e2cb41f15b44266309440ae9aafe7c6c02d71521
SwiftingShadows <jadrenb14@gmail.com>
2016-05-30 20:30:01 +0000

    Add files via upload

---
+++ progression01.lua
@@ -0,0 +1,69 @@
+print( "\n")
+
+local myName = "World"
+print( "Hello " ..myName.. ", Look How Far I've Got!\n" )
+
+print( "\n" )
+
+
+
+local age = 15
+print( "Age - " ..age.. "\n" )
+if age <= 10 then
+	print( "Too Young\n" )
+elseif age >= 150 then
+	print( "Too Old\n")
+else
+	print ( "Please login\n" )
+end
+
+print( " Login - Ja'Dren\n" .. " Password - *******\n " )
+
+myName = "Ja'Dren"
+if myName == "Ja'Dren" then 
+	print( "Welcome Admin " ..myName.. "\n" )
+else
+	print( "ERROR NOT Ja'Dren\n" )
+end
+
+print( "\n")
+
+local age = 39
+print( "Age - " ..age.. "\n" )
+if age <= 10 then
+	print( "Too Young\n" )
+elseif age >= 150 then
+	print( "Too Old\n")
+else
+	print ( "Please login\n" )
+end
+
+print( " Login - Josh\n" .. " Password - ****\n" )
+
+myName = "Josh"
+if myName == "Ja'Dren" then 
+	print( "Welcome Admin " ..myName.. "\n" )
+else
+	print( "ERROR NOT Ja'Dren\n" )
+end
+
+print( "\n" )
+
+local age = 8
+print( "Age - " ..age.. "\n" )
+if age <= 10 then
+	print( "Too Young\n" )
+elseif age >= 150 then
+	print( "Too Old\n")
+else
+	print ( "Please login\n" )
+end
+
+print( " Username - Matt\n" .. " Password - NO ACCESS\n" )
+
+myName = "Matt"
+if myName == "Ja'Dren" then
+	print( "Welcome Admin" ..myName.. "\n" )
+else
+	print( "ERROR NOT Ja'Dren\n" )
+end
